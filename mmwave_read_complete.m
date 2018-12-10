7c7694a16791a697bac16714aab2c181e1b28679
LynnLu <lulilynn@gmail.com>
2018-12-10 11:59:45 -0500

    Update mmwave_read_complete.m

--- mmwave_read_complete.m
+++ mmwave_read_complete.m
@@ -6,16 +6,17 @@ ADCsample_num=256;
 range_bins_num=256; 
 rx_num=4;
 tx_num=1;
+% the number of loops in frameCfg -> may be right
+% according to https://e2e.ti.com/support/sensors/f/1023/t/626801
 chirpsperframe=16; 
+magicword_template=[2;1;4;3;6;5;8;7];
 
 %% read data
-file=fopen('xwr16xx_stream.dat');
-size_frame=[];
+file=fopen('xwr16xx_stream5.dat');
 
 % header
 magicword=fread(file,8,'uint8');
 header=header_read(file);
-size_frame=[size_frame,8+4*7];
 
 package=cell(header{7},3); % 3 means tag,len and payload
 for i=1:header{7}
@@ -29,26 +30,26 @@ dec_obj_num=descriptor(1);
 xyz_q_format=descriptor(2);
 
 %% padding generation
-package_len=cell2mat(package(:,2))+4+4;
-length_all=sum(package_len)+size_frame(1);
-padding=padding_read(file);
+padding=padding_read(file,header,package);
 
 %% following frames
-magicword_template=[2;1;4;3;6;5;8;7];
 headerall=header';
 packageall={package};
 while(~feof(file))
-    magicword=[2;fread(file,7,'uint8')];
+    magicword=fread(file,8,'uint8');
     if(~all(magicword==magicword_template==1))
         break;
     end
     tmpheader=header_read(file);
     tmppackage=cell(tmpheader{7},3); % 3 means tag,len and payload
-    for i=1:header{7}
+    for i=1:tmpheader{7}
         TLV=parseTLV(file,tmpheader{6},range_bins_num,tx_num,rx_num,chirpsperframe);
         tmppackage(i,:)=TLV;
     end
-    padding=padding_read(file);
+    if(length(headerall)==72)
+        a=4;
+    end
+    padding=padding_read(file,tmpheader,tmppackage);
     headerall=[headerall;tmpheader'];
     packageall=[packageall;{tmppackage}];
 end
