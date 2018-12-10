7b843fe34d4221fdda15e0266db526901e1464b3
LynnLu <lulilynn@gmail.com>
2018-12-10 11:58:03 -0500

    Update padding_read.m

--- padding_read.m
+++ padding_read.m
@@ -1,12 +1,12 @@
 %% Author: Li Lu, written in Dec. 2018
-function padding=padding_read(file)
+function padding=padding_read(file,header,package)
 
-padding=fread(file,1,'uint8');
-count=1;
-while(padding~=2)
-    padding=[padding;fread(file,1,'uint8')];
-    count=count+1;
-    if(count>=31)
-        break;
-    end
+len_header=8+4*7+4; % the last 4 is the length of subframe
+package_len=cell2mat(package(:,2))+4+4;
+length_all=sum(package_len)+len_header;
+len_padding=header{2}-length_all;
+if(len_padding~=0)
+    padding=fread(file,len_padding,'uint8');
+else
+    padding=[];
 end
